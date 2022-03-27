3725617bb0a3bbe278b1394f3ee53975732f3fc2
Jacky Liang <asheriffy@gmail.com>
2022-03-27 19:59:09 -0400

    commit tree 33b91c2e736f605fd95751e0ddabd0f491077ba1

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // code your solution here
+const superbowlWin = (arr) => {
+    const hasWon2 = arr.find(el => el.result === 'W')
+
+
+
+    // function hasWin (ob) {
+    //     return ob.result === 'W'
+    // }
+    // const hasWon = arr.find(hasWin)
+    return hasWon2 ? hasWon2.year : hasWon2
+
+
+    // if (hasWon){
+    //     return hasWon.year
+    // }
+    // else{
+    //     return hasWon
+    // }
+
+
+
+    // if (record.result === 'W') {
+    //     return record.year
+    // }
+    // else {
+    //     return undefined
+    // }
+}
+
+// record.find(superbowlWin)
